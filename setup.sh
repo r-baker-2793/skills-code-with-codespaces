963ec4253be5bbc76bdf2cf9bee89fe37ee56baa
r-baker-2793 <ryan.baker@metoffice.gov.uk>
2025-05-20 12:57:55 +0000

    edit the setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
-#! /bin/bash
-# This script installs the sl command on a Linux system.
+#!/bin/bash
 
 sudo apt-get update
 sudo apt-get install sl
-echo "export PATH=$PATH:/usr/games" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
